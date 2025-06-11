8d94d83ace624babea0f5b3a1e4dac0fe955e08e
Mario Garcia Valdez <mariosky@gmail.com>
2025-06-11 14:01:43 -0700

    fundamentos

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,5 +23,6 @@ language = 'es'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 html_static_path = ['_static']
