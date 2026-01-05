626cb40944a300ff88e3a0e00383461b112a6ef7
Mario Garcia-Valdez <mariosky@gmail.com>
2026-01-05 10:29:25 -0800

    pso agent

--- docs/source/fuzzy_code/pso_agent.py
+++ docs/source/fuzzy_code/pso_agent.py
@@ -18,19 +18,27 @@ def remote_ev_controller(particle):
 class SwarmAgent:
     def __init__(self, config) -> None:
         self.config = config
+        self.size = config['dim']
+        self.smin = config['smin']
+        self.smax = config['smax']
+        self.pmax = config['pmax']
+        self.pmin = config['pmin']
+        self.phi1 = config['phi1']
+        self.phi2 = config['phi2']
+
         creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
         creator.create("Particle", list, fitness=creator.FitnessMin, speed=list, smin=None, smax=None, best=None)
 
-    def generate(self, size, pmin, pmax, smin, smax):
-        part = creator.Particle(random.uniform(pmin, pmax) for _ in range(size))
-        part.speed = [random.uniform(smin, smax) for _ in range(size)]
-        part.smin = smin
-        part.smax = smax
+    def generate(self):
+        part = creator.Particle(random.uniform(self.pmin, self.pmax) for _ in range(self.size))
+        part.speed = [random.uniform(self.smin, self.smax) for _ in range(self.size)]
+        part.smin = self.smin
+        part.smax = self.smax
         return part
 
-    def update_particle(self, part, best, phi1, phi2):
-        u1 = (random.uniform(0, phi1) for _ in range(len(part)))
-        u2 = (random.uniform(0, phi2) for _ in range(len(part)))
+    def update_particle(self, part, best):
+        u1 = (random.uniform(0, self.phi1) for _ in range(len(part)))
+        u2 = (random.uniform(0, self.phi2) for _ in range(len(part)))
         v_u1 = map(operator.mul, u1, map(operator.sub, part.best, part))
         v_u2 = map(operator.mul, u2, map(operator.sub, best, part))
         part.speed = list(map(operator.add, part.speed, map(operator.add, v_u1, v_u2)))
@@ -51,12 +59,24 @@ class SwarmAgent:
         for i, part in enumerate(self.pop):
             part.fitness.values = results[i]
         return self.pop
+    
+    def migrate(self, migration_particles):
+        self.pop.sort(key=lambda p: p.fitness.values)
+        particles = [] 
+        for x, f in migration_particles:
+            part = self.generate()
+            part[:] = x 
+            part.fitness.values = f 
+            particles.append(part)
+        
+        self.pop = self.pop[:-len(particles)] + particles
+
 
     def main(self, config):
         toolbox = base.Toolbox()
-        toolbox.register("particle", self.generate, size=config['dim'], pmin=-0.05, pmax=1.5, smin=-0.1, smax=0.1)
+        toolbox.register("particle", self.generate)
         toolbox.register("population", tools.initRepeat, list, toolbox.particle)
-        toolbox.register("update", self.update_particle, phi1=2.0, phi2=2.0)
+        toolbox.register("update", self.update_particle)
         self.pop = toolbox.population(n=config['swarm_size'])
 
         stats = tools.Statistics(lambda ind: ind.fitness.values)
@@ -84,8 +104,9 @@ class SwarmAgent:
                 toolbox.update(part, best)
             # Gather all the fitnesses in one list and print the stats
             logbook.record(gen=g, evals=len(self.pop), **stats.compile(self.pop))
-            print(logbook.stream)
-            
+            # print(logbook.stream)
+        
+    
         #print(best.fitness.values, best)
         #return self.pop, logbook, best
         return best, logbook
@@ -106,36 +127,39 @@ config = {
     "migrate_interval":2,
     "migrate_k":2,
     "num_swarms": 6,
-    "num_rounds": 4,
+    "num_rounds": 8,
 }
 
-ray.init(ignore_reinit_error=True)
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+creator.create("Particle", list, fitness=creator.FitnessMin, speed=list, smin=None, smax=None, best=None)
+
+ray.init(ignore_reinit_error=True, include_dashboard=False)
 try:
    agents = [SwarmAgent.remote(config) for i in range(config['num_swarms'])
    ]
-
+   
+   best_global = None
    for r in range(config["num_rounds"]):
        # 1) Cada agente ejecuta varias iteraciones locales en paralelo
        futures = [a.main.remote(config) for a in agents]
-       bests = ray.get(futures)  # [(f, x), ...]
+       bests = [best for best, _ in ray.get(futures)]  # [(f, x), ...]
+       bests.sort(key=lambda best: best.fitness.values) 
+
 
        # 2) Actualizar mejor global
-       for best, log in bests:
-           print(best.fitness.values, best)
-
-       #print(f"Ronda {r:02d} | mejor global = {best_global_f:.6f}")
-
+       best = bests[0]
+       if best_global is None or best_global.fitness < best.fitness:
+           best_global = creator.Particle(best)
+           best_global.fitness.values = best.fitness.values
+       print(f"Ronda {r:02d} | mejor global = {best_global.fitness.values[0]:.6f} | {best_global}")
+       
        # 3) Migración: intercambio de candidatos entre agentes
-       #if (r + 1) % migrate_every == 0:
-           # Tomar los mejores de cada agente
-       #    elites = [x for (f, x) in bests]
-           # Enviar a cada agente candidatos de otros agentes
-       #    mig_futures = []
-       #    for i, a in enumerate(agents):
-               # Candidatos: mejores de los otros (rotación simple)
-       #        others = [elites[(i + j) % n_agents] for j in range(1, n_agents)]
-        #       mig_futures.append(a.inject_candidates.remote(others[:migrate_k]))
-        #   ray.get(mig_futures)
+       migration_futures = []
+       migration_particles = [(list(p), p.fitness.values) for p in bests[:config['migrate_k']]]
+       for a in agents:
+           # Candidatos: enviamos los k mejores a los otros agentes 
+           migration_futures.append(a.migrate.remote(migration_particles))
+       ray.get(migration_futures)
 
 finally:
    ray.shutdown()
